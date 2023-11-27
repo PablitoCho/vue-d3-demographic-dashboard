4788bb382833fab6e2b4695cd984bc1354dc5487
woncho <woncho@gmarket.com>
2023-11-27 12:26:30 +0900

    dashboard layout setup

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-8 px-0">
-    <div class="alert alert-danger" style="height: 20rem;">Bar Chart Area</div>
+    <div class="alert alert-danger" style="height: 25rem;">Bar Chart Area</div>
   </div>
 </template>
 
