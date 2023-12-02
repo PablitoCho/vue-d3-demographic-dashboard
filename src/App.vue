e66f4f18f7ce5e6cb36a4cf3d8263157d385548c
woncho <woncho@gmarket.com>
2023-12-02 13:52:43 +0900

    axio

--- src/App.vue
+++ src/App.vue
@@ -18,10 +18,20 @@ import SearchBar from '@/components/SearchBar.vue'
 import LineChart from '@/components/charts/LineChart.vue'
 import BarChart from '@/components/charts/BarChart.vue'
 import PieChart from '@/components/charts/PieChart.vue'
+import { getBirthRates, getDemographicValues } from '@/services'
 
-const fetchDemographicData = () => console.log('hi')
 
-// import { fetchDemographicData } from '@/services'
+const fetchDemographicData = () => {
+  const result = getBirthRates(['Seoul', 'Busan', 'Jeju']);
+  result
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
+
+  const result2 = getDemographicValues('Death');
+  result2
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
+}
 
 </script>
 
